8d2d678d0a91cffcd5ffe78ef65261fb4c2ad6c3
Ariel Pchara <ariel@pchara.com>
2018-09-04 09:54:29 -0300

    navigation

--- front/src/containers/App.js
+++ front/src/containers/App.js
@@ -1,5 +1,6 @@
-import React, { Component } from 'react';
-import { BrowserRouter, Switch, Route, Link } from 'react-router-dom';
+import React from 'react';
+import { BrowserRouter, Switch, Route, NavLink } from 'react-router-dom';
+
 import Todo from '../containers/Todo';
 import withService from '../utils/withService';
 import TodoService from '../services/todo.service';
@@ -8,22 +9,27 @@ const todoServiceTodo = new TodoService('todo');
 const todoServiceTask = new TodoService('task');
 
 function App(props) {
-    return (
-        <>
-            <BrowserRouter>
-                <>
-                <div>
-                    <Link to="/">Todo</Link> | 
-                    <Link to="/tasks">Tasks</Link>
-                </div>
-                <Switch>
-                    <Route path="/tasks" component={withService(todoServiceTask, 'Task List')(Todo)} />
-                    <Route path="/" component={withService(todoServiceTodo, 'Todo List')(Todo)} />
-                </Switch>
-                </>
-            </BrowserRouter>
-        </>
-    )
+  return (
+    <BrowserRouter>
+      <>
+        <nav className="nav justify-content-end">
+          <NavLink className="nav-link" exact to="/" activeClassName="active">
+            Todo
+          </NavLink>
+          <NavLink className="nav-link" exact to="/tasks" activeClassName="active">
+            Tasks
+          </NavLink>
+          <NavLink className="nav-link" exact to="/about" activeClassName="active">
+            Sobre
+          </NavLink>
+        </nav>
+        <Switch>
+          <Route path="/tasks" component={withService(todoServiceTask, 'Task List')(Todo)} />
+          <Route path="/" component={withService(todoServiceTodo, 'Todo List')(Todo)} />
+        </Switch>
+      </>
+    </BrowserRouter>
+  );
 }
 
 export default App;
